1d0c680e0a227fb44f9771949ce96ddcd8c706c0
safronman <safronmanbox@gmail.com>
2020-10-13 14:04:34 +0300

    NgComponentOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { ThankYouComponent } from './components/thank-you/thank-you.component';
 
 @Component({
     selector: 'app-root',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    dynamicTabs = [
+        {
+            label: 'User Information',
+            component: UserInfoComponent
+        },
+        {
+            label: 'Payment',
+            component: PaymentComponent
+        },
+        {
+            label: 'Thank You',
+            component: ThankYouComponent
+        }
+    ];
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { UiACounterComponent } from './components/ui-a-counter/ui-a-counter.component';
 import { UiBCounterComponent } from './components/ui-b-counter/ui-b-counter.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { ThankYouComponent } from './components/thank-you/thank-you.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material';
 
 @NgModule({
     declarations: [
         AppComponent,
         CounterComponent,
         UiACounterComponent,
-        UiBCounterComponent
+        UiBCounterComponent,
+        UserInfoComponent,
+        PaymentComponent,
+        ThankYouComponent,
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        NoopAnimationsModule,
+        MatTabsModule
+    ],
+    entryComponents: [
+        UserInfoComponent,
+        PaymentComponent,
+        ThankYouComponent
     ],
     providers: [],
     bootstrap: [AppComponent]
